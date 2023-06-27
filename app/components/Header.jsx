b81b4925c884859e4bfb53a4d774f9188ecc0348
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-06-27 17:19:06 +0200

    Header for children of subdir.

---
+++ app/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NameIcon } from './NameIcon'
+import Navbar from '../Navbar'
+
+export default function Header() {
+    return (
+        <div>
+            <NameIcon />
+            <Navbar />
+        </div>
+    )
+}
