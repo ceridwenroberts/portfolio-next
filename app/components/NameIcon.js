a026e8550dc9a45d8497f69119d0ba8b4b0a3b65
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-06-28 11:32:28 +0200

    update conitional routing to home on icon

--- app/components/NameIcon.js
+++ app/components/NameIcon.js
@@ -5,16 +5,11 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export const NameIcon = () => {
-  let activeLink = "";
   const pathname = usePathname();
-
-pathname != "./" ?? activeLink === "./"
-
-// console.log({activeLink});
 
   return (
     <>
-    <Link href={activeLink}>
+    <Link href={pathname != "/" ? "./" : ""}>
       <div>Ceridwen Roberts</div>
       <div>
         <p>creative -- developer</p>
