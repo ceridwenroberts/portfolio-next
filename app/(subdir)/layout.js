8393a5f5259d7fb45982f39ab512c07b75febe55
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-08-06 15:35:45 +0200

    fix subdir module import

--- app/(subdir)/layout.js
+++ app/(subdir)/layout.js
@@ -1,7 +1,8 @@
 import { NameIcon } from "../components/NameIcon";
 import Navbar from "../Navbar";
 import Navi from "../Navi";
-import styles from "app/(subdir)/subdir.module.css";
+// import styles from "app/(subdir)/subdir.module.css";
+import styles from "./subdir.module.css"
 import Header from "@/app/components/Header";
 
 export default function SubdirLayout({ children }) {
