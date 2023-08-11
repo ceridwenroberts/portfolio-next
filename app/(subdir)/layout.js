cbb2ba1cd9f1accb969059d9d69585e547a031af
ceridwenroberts <ceridwen.roberts@gmail.com>
2023-08-11 11:48:20 +0200

    delete for deployment

--- app/(subdir)/layout.js
+++ app/(subdir)/layout.js
@@ -1,13 +1,13 @@
 import { NameIcon } from "../components/NameIcon";
 import Navbar from "../Navbar";
 import Navi from "../Navi";
-import styles from "./subdir.module.css"
+
 import Header from "@/app/components/Header";
 
 export default function SubdirLayout({ children }) {
   return (
     <>
-      <div className={styles.main}>
+      <div>
         <Header />
       </div>
       {children}
